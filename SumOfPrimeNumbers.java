da717ad6f86bd53a5a9a83cd61f54da281cdd377
mohanprasathcsbs <mohanprasath.n2023csbs@sece.ac.in>
2026-06-17 09:09:41 +0530

    q2

---
+++ SumOfPrimeNumbers.java
@@ -0,0 +1,30 @@
+public class SumOfPrimeNumbers {
+    public static void main(String[] args) {
+
+        int count = 0;
+        int num = 2;
+        int sum = 0;
+
+        while (count < 100) {
+            if (isPrime(num)) {
+                sum += num;
+                count++;
+            }
+            num++;
+        }
+
+        System.out.println("Sum of the first 100 prime numbers: " + sum);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
