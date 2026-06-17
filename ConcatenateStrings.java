db4a661150576e445cdd7bd00291c72d42ba4150
mohanprasathcsbs <mohanprasath.n2023csbs@sece.ac.in>
2026-06-17 09:10:24 +0530

    q3

---
+++ ConcatenateStrings.java
@@ -0,0 +1,11 @@
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+
+        String str1 = "Python";
+        String str2 = "Tutorial";
+
+        String result = str1.substring(1) + str2.substring(1);
+
+        System.out.println(result);
+    }
+}
